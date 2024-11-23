374233ab4aaa88c5c4d0d215e9840005417086ba
Ruslan9222 <magog21@gmail.com>
2024-11-23 23:13:13 +0300

    a_loop_for_program_execution

--- console-sorter/src/main/java/sorter/project/entity/Barrel.java
+++ console-sorter/src/main/java/sorter/project/entity/Barrel.java
@@ -1,28 +1,28 @@
 package sorter.project.entity;
 
-public class Barrel implements SelfComparator<Barrel>{
-	private float volume;
-	private String matherialOfPreserve;
-	private String matherial;
-	
-	public Barrel(float volume, String matherialOfPreserve, String matherial) {
-		super();
-		this.volume = volume;
-		this.matherialOfPreserve = matherialOfPreserve;
-		this.matherial = matherial;
-	}
+public class Barrel implements SelfComparator<Barrel> {
+    private float volume;
+    private String matherialOfPreserve;
+    private String matherial;
 
-	@Override
-	public boolean moreThen(Barrel another) {
-		// TODO Auto-generated method stub
-		return (this.volume - another.volume) > 0.00001;
-	}
+    public Barrel(float volume, String matherialOfPreserve, String matherial) {
+        super();
+        this.volume = volume;
+        this.matherialOfPreserve = matherialOfPreserve;
+        this.matherial = matherial;
+    }
 
-	@Override
-	public String toString() {
-		return "Barrel [volume=" + volume + ", matherialOfPreserve=" + matherialOfPreserve + ", matherial=" + matherial
-				+ "]";
-	}
-	
+    @Override
+    public boolean moreThen(Barrel another) {
+        // TODO Auto-generated method stub
+        return (this.volume - another.volume) > 0.00001;
+    }
+
+    @Override
+    public String toString() {
+        return "Barrel [volume=" + volume + ", matherialOfPreserve=" + matherialOfPreserve + ", matherial=" + matherial
+                + "]";
+    }
+
 
 }
