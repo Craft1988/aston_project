8966a2497c17f5f918b86207ffe625013e6923b5
Kirill <katofrag@yandex.ru>
2024-11-23 21:52:18 +0700

    create builder object

--- console-sorter/src/main/java/sorter/project/MainClass.java
+++ console-sorter/src/main/java/sorter/project/MainClass.java
@@ -1,26 +1,17 @@
 package sorter.project;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import sorter.project.entity.Animal;
 import sorter.project.entity.Barrel;
-import sorter.project.utils.Sorter;
+import sorter.project.entity.Human;
 
 public class MainClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Animal[] animals = { new Animal("Herbivore", true), new Animal("Pet", true), new Animal("Wild", false),
-				new Animal("Predator", true), new Animal("Any", true) };
-		Barrel[] barrels = { new Barrel(5.0f, "Wine", "Wooden"), new Barrel(2.0f, "Oil", "Plastic"),
-				new Barrel(1.5f, "Beer", "Metall") };
-
-		Sorter.sortAnyCustomClass(animals);
-		System.out.println(Arrays.toString(animals));
-		Sorter.sortAnyCustomClass(barrels);
-		System.out.println(Arrays.toString(barrels));
-		Animal an = animals[4];
-		System.out.println(Sorter.binarySearch(an, animals));
-	}
+    public static void main(String[] args) {
+        // тестим создание объекта билдером
+        Animal animal = new Animal.AnimalBuilder("Bird", "Black", false).build();
+        Barrel barrel = new Barrel.BarrelBuilder(1.1, "Gold", "Iron").build();
+        Human human = new Human.HumanBuilder("men", 55, "Tarantino").build();
+        System.out.println(animal);
+        System.out.println(barrel);
+        System.out.println(human);
+    }
 }
