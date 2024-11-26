40fe9da916ecd8b56f9f631b619544b12ead2e13
Notebook <magog21@gmail.com>
2024-11-26 19:59:21 +0300

    sorter_fix

--- console-sorter/src/main/java/sorter/project/utils/InsertionSort.java
+++ console-sorter/src/main/java/sorter/project/utils/InsertionSort.java
@@ -21,19 +21,5 @@ public final class InsertionSort<T> implements SortStrategy<T> {
 }
 
 
-//    public static <T> void insertionSort(List<T> unsorted, Comparator<? super T> c) {
-//        for (int i = 1; i < unsorted.size(); i++) {
-//            for (int j = i; j > 0; j--) {
-//                if (c.compare(unsorted.get(j - 1), unsorted.get(j)) > 0) {
-//                    T temp = unsorted.get(j - 1);
-//                    unsorted.remove(j - 1);
-//                    unsorted.add(j, temp);
-//                }
-//            }
-//        }
-//    }
-//
-//    public static <T> int binarySearch(T item, T[] src) {
-//        return -1;
-//    }
+
 
